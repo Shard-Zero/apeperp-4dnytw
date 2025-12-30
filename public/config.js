268904fa56d833629adae3c39b260e9db72cda8e
Aiman.eth <superalphadao@gmail.com>
2025-12-30 13:22:37 +0800

    Configure DEX - public/config.js

---
+++ public/config.js
@@ -0,0 +1,6 @@
+window.__RUNTIME_CONFIG__ = {
+  "VITE_APP_NAME": "ApePerp",
+  "VITE_APP_DESCRIPTION": "A powerful perpetual trading DEX powered by Orderly Network",
+  "VITE_ORDERLY_BROKER_NAME": "ApePerp",
+  "VITE_BASE_URL": "/apeperp-4dnytw/"
+};
